3c2f52d2776073eae486cb2c670d17979acc708b
austin <austinlau94@163.com>
2023-04-18 18:36:03 +0800

    modify

--- api/index.py
+++ api/index.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
 
-app = Flask(__name__, static_url_path='', root_path='../static')
+app = Flask(__name__, static_url_path='/static/', root_path=r'D:\BaiduNetdiskDownload\GitBook')
 
 
-@app.route("/api/function/", methods=["GET"])
+@app.route('/index/')
 def index():
     return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8890)
